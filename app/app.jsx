aca3c1f233dbf34cdc571681728d2bb751cfc4c4
nadir B <hecceite@free.fr>
2021-02-17 01:05:18 +0100

    ready heroku app

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,51 @@
 const React = require("react");
+const useState = React.useState;
 const ReactDOM = require("react-dom");
+const namer = require("./util/namer");
+const Greeting = require("./components/Greeting");
 
-/* Import Components */
-const RootComponent = require("./components/RootComponent");
 
-ReactDOM.render(<RootComponent/>, document.getElementById("main"));
+const last = namer.last();
+
+const RootComponent = () => {
+
+    const [first, setFirst] = useState(namer.first);
+    const [last, setLast] = useState(namer.last);
+    const [lang, setLang] = useState("fr");
+
+    const refreshFirst = () => {
+        setFirst(namer.first());
+    }
+    const refreshLast = () => {
+        setLast(namer.last());
+    }
+    
+    const switchLang = () => {
+        if (lang === "en") {
+            setLang("fr");
+        } else {
+            setLang("en");
+        }
+    }
+
+    let langButton;
+    if (lang === "en"){
+        langButton = <button onClick={switchLang}>Switch to French</button>
+    } else {
+        langButton = <button onClick={switchLang}>Switch to English</button>
+    }
+
+    return (
+        <div>
+            <Greeting first={first} last={last} lang={lang}/>
+            <button onClick={refreshFirst}>Refresh first name</button>
+            <button onClick={refreshLast}>Refresh last name</button>
+            {langButton}
+        </div>
+    );
+}
+
+ReactDOM.render(
+     <RootComponent />,
+    document.getElementById("main")
+);
